a666d6877163f77ed02069a0c036e8c720f627db
Václav Parma <vencacomp@gmail.com>
2023-04-11 10:29:21 +0200

    login v1

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Flutter aplikace pro ukládání hesel
 
 ## Záznam počtu odpracovaných hodin
 
-5.4. - 1h 
+5.4. - 1.5h 
+10.4. - 1h
 -----
-Celkem: 1h
+Celkem: 1.5h
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'dart:async';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
 import 'package:local_auth/local_auth.dart';
 
@@ -13,78 +14,250 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter password manager',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: 
-        const MyHomePage(title: 'Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final LocalAuthentication auth = LocalAuthentication();
-  bool _supportState = false;
-
-  @override
-  void initState() {
-    super.initState();
-    auth.isDeviceSupported().then(
-      (bool isSupported) => setState(() {
-        _supportState = isSupported;
-      })
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (_supportState)
-              const Text("Supported")
-            else
-              const Text("Not supported"),
-
-            const Divider(height: 100),
-
-            ElevatedButton(
-              onPressed: _authenticate, 
-              child: const Text("Authenticate")
-              )
-          ],
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Password manager',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         ),
+        home: const MainPage(),
       ),
     );
   }
+}
 
-  Future<bool> _authenticate() async {
+class MyAppState extends ChangeNotifier {
+  var authed = false;
+
+  void authenticate() async {
+    final LocalAuthentication auth = LocalAuthentication();
+
     try{
       bool authenticated = await auth.authenticate(
         localizedReason: "Přihlášení",
         options: const AuthenticationOptions(
           stickyAuth: true,
-          biometricOnly: false
+          biometricOnly: true
         )
       );
-      return authenticated;
+      if (authenticated){
+        authed = authenticated;
+        notifyListeners();
+      }
     }on PlatformException catch(e){
-      return false;
+      authed = false;
     }
+  }
+
+  var pin = {};
+
+  void enter_pin(pin_value, location) async {
+    pin[location] = pin_value;
+    print(pin);
+    notifyListeners();
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPage();
+}
+
+class _MainPage extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var authed = appState.authed;
+
+    if (authed){
+      return HomePage();
+    }
+    else{
+      return LoginPage();
+    }
+  }
+}
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
+    appState.authenticate();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const Text("Zadejte pin, nebo použijte biometrické ověření"),
+              PinInput(),
+              ElevatedButton(
+                onPressed: (){
+                  appState.authenticate();
+                }, 
+                child: const Text("Použít biometriku")
+              ),
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
+
+class PinInput extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
+    return Form(
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              height: 48,
+              width: 44,
+              child: TextFormField(
+                onSaved: (newValue) {
+                  appState.enter_pin(newValue, 0);
+                },
+                onChanged: (value) {
+                  if (value.length == 1){
+                    FocusScope.of(context).nextFocus();
+                  }
+                },
+                style: Theme.of(context).textTheme.titleLarge,
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(1),
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 48,
+              width: 44,
+              child: TextFormField(
+                onSaved: (newValue) {
+                  appState.enter_pin(newValue, 1);
+                },
+                onChanged: (value) {
+                  if (value.length == 1){
+                    FocusScope.of(context).nextFocus();
+                  }
+                },
+                style: Theme.of(context).textTheme.headline6,
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(1),
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 48,
+              width: 44,
+              child: TextFormField(
+                onSaved: (newValue) {
+                  appState.enter_pin(newValue, 2);
+                },
+                onChanged: (value) {
+                  if (value.length == 1){
+                    FocusScope.of(context).nextFocus();
+                  }
+                },
+                style: Theme.of(context).textTheme.headline6,
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(1),
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 48,
+              width: 44,
+              child: TextFormField(
+                onSaved: (newValue) {
+                  appState.enter_pin(newValue, 3);
+                },
+                onChanged: (value) {
+                  if (value.length == 1){
+                    FocusScope.of(context).nextFocus();
+                  }
+                },
+                style: Theme.of(context).textTheme.headline6,
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(1),
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 48,
+              width: 44,
+              child: TextFormField(
+                onSaved: (newValue) {
+                  appState.enter_pin(newValue, 4);
+                },
+                onChanged: (value) {
+                  if (value.length == 1){
+                    FocusScope.of(context).nextFocus();
+                  }
+                },
+                style: Theme.of(context).textTheme.headline6,
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(1),
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 48,
+              width: 44,
+              child: TextFormField(
+                onSaved: (newValue) {
+                  appState.enter_pin(newValue, 5);
+                },
+                onChanged: (value) {
+                  if (value.length == 1){
+                    FocusScope.of(context).nextFocus();
+                  }
+                },
+                style: Theme.of(context).textTheme.headline6,
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(1),
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SafeArea(
+        child: const Text("HomePage")
+      )
+    );
   }
 }
